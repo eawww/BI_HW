55edab736721c1d2654cbd7472a48f11fae5d4fd
Eric Wilson <eawww0@gmail.com>
2016-10-11 17:57:32 -0500

    Started on Q2. Most of everything else is in pseudocode comments

--- HW2/Hw2-Q2-1.py
+++ HW2/Hw2-Q2-1.py
@@ -17,9 +17,26 @@ def main():
 
     #map kmers into number of occurrences of each possible kmer
     #build 2 dictionaries of every possible combination of characters
+    poss_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M',
+                 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    PMF = {}
+    for i in range(0, len(poss_char)-1):
+        for j in range(0, len(poss_char)-1):
+            key = str(poss_char[i] + poss_char[j])
+            PMF[key] = [0, 0]
+
     #iterate over each set of kmers, incrementing corresponding value in corresponding dictionary for each kmer
-    #eliminate unneeded dictionary pairs that are not in either dictionary
+    for sequence in sequence_kmers:
+        for kmer in sequence_kmers[sequence]:
+            PMF[kmer][sequence] += 1
+
+    #eliminate unneeded dictionary pairs that are not in either dictionary (optional)
         #both dictionaries should be the same length at this point
+    for k, v in PMF.items():
+        if(v == [0,0]):
+            del PMF[k]
+        else:
+            PMF[k][0] = float((PMF[k][0] - PMF[k][1])) #this needs to be absolute value or squared
     #for each key in either dictionary, change value to float(value/number of kmers in that sequence)
     #find sum for each key (valueA - B)^2  or ABS(valueA - valueB) -- this will be our score
     #output score
