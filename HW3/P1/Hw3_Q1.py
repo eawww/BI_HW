608267ce5787f2a7b4dd529ffd8fe0a7641b3b92
Eric Wilson <eawww0@gmail.com>
2016-10-23 16:00:27 -0500

    break up script into functions

--- HW3/P1/Hw3_Q1.py
+++ HW3/P1/Hw3_Q1.py
@@ -18,26 +18,50 @@ fout = open(str(OutputFile), "w")
 poss_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M',
              'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-#Dictionaries for substitution matrix and counts
+#Dictionaries for substitution matrix, counts, and sequences
 AASM = {}
 counts = {}
 sequences = []
 
-#Populate the dictionaries
 
-for character in poss_char:
-    counts[character] = 0
-    AASM[character] = {}    #Nested dictionaries make up the matrix
-    for character2 in poss_char:
-        AASM[character][character2] = 0
+def main():
+    populateDictionaries()
+    parseInput()
+    #use counts and sequences to gather counts of each individual character
+    #use AASM as temporary storage for pair counts
+    #calculate values for each entry in table and replace pair count with
 
-#read in and parse sequences into usable data
-#assumes all sequences are delimited by '>' line at top and empty line at bottom
-tempstrings = []
-for line in fin.readlines():
-    if line[0] == '>':
-        print("ignoring heading line")
-    elif (line == "\n"):
-        #denotes end of sequence
-    else:
-        tempstrings.append(line)
+
+# read in and parse sequences into usable data
+# assumes all sequences are delimited by '>' line at top and empty line at bottom
+def parseInput():
+    tempstrings = []
+    templine = ""
+    for line in fin.readlines():
+        if line[0] == '>':
+            print("Reading aligned sequence " + line)
+        elif (line == "\n"):
+            # denotes end of sequence
+            # mash them all into one string and add it to sequences
+            assembledsequence = ""
+            for substring in tempstrings:
+                assembledsequence += substring
+            sequences.append(assembledsequence)
+            # set tempstrings back to []
+            tempstrings = []
+        else:
+            templine = line.strip()
+            templine.replace(" ", "")
+            tempstrings.append(line)
+
+
+def populateDictionaries():
+    # Populate the dictionaries
+    for character in poss_char:
+        counts[character] = 0
+        AASM[character] = {}  # Nested dictionaries make up the matrix
+        for character2 in poss_char:
+            AASM[character][character2] = 0
+
+#call main function to start program
+main()
